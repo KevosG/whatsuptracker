aea1798bf1b0d703323075d8a5f6c7487b7d5c39
Kevin Grieco <kevin.grieco@gmail.com>
2021-07-04 21:30:59 -0400

    added clear and list functionality

--- app.py
+++ app.py
@@ -1,29 +1,62 @@
-
-
-
-# # Create database if it doesn't exist, need to add conditional later
-# conn = sqlite3.connect('whatsup.db')
-# c = conn.cursor()
-# c.execute('''CREATE TABLE ENTRIES
-#              ([Date] date,[Activity] text)''')
-
-
-#  MENU
-# 1 - Start timer
-#   1a - How often to prompt?
-#   1b - How long to prompt for?
-# 2 - View Entries
-#   2a - Today Only
-#   2b - All Entries
-# 3 - Export to CSV
-# 3 - Clear Entries
-#   3a - Are you sure?
-# 4 - Quit
-
-import sqlite3, beepy
+import sqlite3, beepy, time
 from sqlite3 import Error
-from datetime import datetime
-import time
+from datetime import datetime, timedelta
+from pathlib import Path
+
+db_path = Path("./whatsup.db")
+database = r"whatsup.db"
+
+if db_path.is_file():
+    pass
+else:
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE ENTRIES
+                ([Date] date,[Activity] text)''')
+
+def top_menu():
+    choice = input("Enter a selection.\n\n[1] - Start Timer\n[2] - View Entries\n[3] - Clear Entries\n[4] - Quit\n\n> ")
+    if choice == "1":
+        configure_timer()
+    if choice == "2":
+        entries = get_posts()
+        for row in entries:
+            print(row)
+        top_menu()
+    if choice == "3":
+        clear_posts()
+        top_menu()
+    if choice == "4":
+        quit()
+
+def configure_timer():
+    interval = int(input("How often to prompt? Press Enter to accept default of once every 20 minutes.\n\n[20] >") or "20")
+    duration = int(input("For how many hours do you want to do this to yourself?  Press Enter to accept default of 2 hours. \n\n[2] >") or "2")
+    print(f"Prompting every { interval } minutes for { duration } hours.")
+    run_timer(duration, interval)
+
+def run_timer(duration, interval):
+    conn = create_connection(database)
+    stop_time = datetime.now() + timedelta(hours=duration)
+    while datetime.now() < stop_time:
+        with conn:
+            status_update = get_status()
+            update_entries(conn, status_update)
+        entries = get_posts()
+        for row in entries:
+            print(row)
+        time.sleep(interval * 60)
+
+def get_status():
+    now = datetime.now()
+    formatted_now = now.strftime("%m/%d/%Y, %H:%M")
+    try:
+        beepy.beep(sound = "coin")
+    except:
+        pass
+    update = input("What have you been doing for the last 20 minutes?\n\n> ")
+    status_update = (formatted_now, update)
+    return status_update
 
 def create_connection(db_file):
     conn = None
@@ -31,15 +64,20 @@ def create_connection(db_file):
         conn = sqlite3.connect(db_file)
     except Error as e:
         print(e)
-
     return conn
 
-def get_posts(conn):
+def get_posts():
+    conn = create_connection(database)
     cur = conn.cursor()
     cur.execute('select * from entries')
     posts = cur.fetchall()
-
     return posts
+
+def clear_posts():
+    conn = create_connection(database)
+    cur = conn.cursor()
+    cur.execute('DELETE FROM entries')
+    conn.commit()
 
 def update_entries(conn, status_update):
     sql = ''' INSERT INTO entries(date,activity)
@@ -47,29 +85,7 @@ def update_entries(conn, status_update):
     cur = conn.cursor()
     cur.execute(sql, status_update)
     conn.commit()
-
     return cur.lastrowid
 
-def get_status():
-    now = datetime.now()
-    formatted_now = now.strftime("%m/%d/%Y, %H:%M")
-    beepy.beep(sound = "coin")
-    update = input("What have you been doing for the last 20 minutes?\n\n--> ")
-    status_update = (formatted_now, update)
-
-    return status_update
-
-def main():
-    while True:
-        database = r"whatsup.db"
-        conn = create_connection(database)
-        with conn:
-            status_update = get_status()
-            update_entries(conn, status_update)
-        entries = get_posts(conn)
-        for row in entries:
-            print(row)
-        time.sleep(1200)
-
 if __name__ == '__main__':
-    main()
+    top_menu()
